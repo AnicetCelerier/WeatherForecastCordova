fa93db354595172099e480edbc3f1fc0170e0cd1
AnicetCelerier <anicet.celerier@gmail.com>
2022-09-07 17:30:16 +0200

    camera

--- www/js/index.js
+++ www/js/index.js
@@ -25,16 +25,66 @@ function onDeviceReady() {
   // Cordova is now initialized. Have fun!
 
   console.log("Running cordova-" + cordova.platformId + "@" + cordova.version);
-//   document.getElementById("deviceready").classList.add("ready");
-//   window.addEventListener("batterystatus", onBatteryStatus, false);
+  //   document.getElementById("deviceready").classList.add("ready");
+  window.addEventListener("batterystatus", onBatteryStatus, false);
   //   window.addEventListener("batterylow", onBatteryStatus, false);
   //   window.addEventListener("batterycritical", onBatteryStatus, false);
-  console.log("Level: " + cordova.level + " isPlugged: " + cordova.isPlugged ); 
-
 }
 
-window.addEventListener("batterystatus", onBatteryStatus, false);
+// window.addEventListener("batterystatus", onBatteryStatus, false);
 
 function onBatteryStatus(status) {
-    console.log("Level: " + status.level + " isPlugged: " + status.isPlugged);
+  console.log("Level: " + status.level + " isPlugged: " + status.isPlugged);
+}
+
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+  console.log("navigator.geolocation works well");
+  navigator.geolocation.getCurrentPosition(
+    geolocationSuccess,
+    [geolocationError],
+    [geolocationOptions]
+  );
+}
+
+var onSuccess = function (position) {
+  alert(
+    "Latitude: " +
+      position.coords.latitude +
+      "\n" +
+      "Longitude: " +
+      position.coords.longitude +
+      "\n" +
+      "Altitude: " +
+      position.coords.altitude +
+      "\n" +
+      "Accuracy: " +
+      position.coords.accuracy +
+      "\n" +
+      "Altitude Accuracy: " +
+      position.coords.altitudeAccuracy +
+      "\n" +
+      "Heading: " +
+      position.coords.heading +
+      "\n" +
+      "Speed: " +
+      position.coords.speed +
+      "\n" +
+      "Timestamp: " +
+      position.timestamp +
+      "\n"
+  );
+};
+
+// onError Callback receives a PositionError object
+//
+function onError(error) {
+  alert("code: " + error.code + "\n" + "message: " + error.message + "\n");
+}
+
+navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+  console.log(navigator.camera);
 }
